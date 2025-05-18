727281aecd29b7d7afe87d13df8b60d8b12dce94
raphdf201 <raphdf201@hotmail.com>
2025-05-18 12:55:38 -0400

    add hashing script

---
+++ hash.ps1
@@ -0,0 +1,6 @@
+﻿param(
+  [Parameter(Mandatory = $true)]
+  [string] $file
+)
+
+certutil -hashfile $file SHA256
---
+++ vs.ps1
@@ -0,0 +1,2 @@
+﻿cd "C:\Program Files (x86)\Microsoft Visual Studio\Installer"
+.\vs_installer.exe --locale en-US
